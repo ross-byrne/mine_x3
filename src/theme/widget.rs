b2bdd94aa74c2ef30ff8afab3a0f47a21303ad83
Ross Byrne <ross.byrne@outlook.ie>
2025-06-07 17:03:31 +0200

    Updated border radius for buttons

--- src/theme/widget.rs
+++ src/theme/widget.rs
@@ -67,7 +67,7 @@ where
                 justify_content: JustifyContent::Center,
                 ..default()
             },
-            BorderRadius::MAX,
+            BorderRadius::all(Val::Px(24.0)),
         ),
     )
 }
